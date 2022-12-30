e5cadde714208632a243904805f3378be70c95a8
Yassine El Haddad <megahaddad15@gmail.com>
2022-12-30 18:41:55 +0100

    Fixed Issues and improved script logic

--- flutter_init.sh
+++ flutter_init.sh
@@ -3,18 +3,34 @@
 #                                                         :::      ::::::::    #
 #    flutter_init.sh                                    :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: ahmaymou <ahmaymou@student.42.fr>          +#+  +:+       +#+         #
+#    By: yel-hadd <yel-hadd@mail.com>               +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/11/29 18:58:43 by yel-hadd          #+#    #+#              #
-#    Updated: 2022/11/30 13:11:17 by ahmaymou         ###   ########.fr        #
+#    Updated: 2022/12/30 18:40:21 by yel-hadd         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-username = "yel-hadd"
-rm -rf "/goinfre/$username/flutter"
-mkdir -p "/goinfre/$username/flutter"
-curl "https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.3.9-stable.zip" -o "/goinfre/$username/flutter.zip"
-unzip "/goinfre/$username/flutter.zip" -d "/goinfre/$username/"
-alias flutter='/goinfre/$username/flutter/bin/flutter'
-alias dart='/goinfre/$username/flutter/bin/dart'
-rm -f "/goinfre/$username/flutter.zip"
+FLUTTER_DL_LINK="https://storage.googleapis.com/flutter_infra_release/releases/stable/macos/flutter_macos_3.3.9-stable.zip"
+USER=$(whoami)
+if [ ! -d "/goinfre/$USER/flutter" ]; then
+  mkdir -p "/goinfre/$USER/flutter"
+  curl "$FLUTTER_DL_LINK" -o "/goinfre/$USER/flutter.zip"
+  unzip "/goinfre/$USER/flutter.zip" -d "/goinfre/$USER/"
+  rm -f "/goinfre/$USER/flutter.zip"
+fi
+
+if grep -Fq "flutter='/goinfre/$USER/flutter/bin/flutter'" ~/.zshrc
+then
+  :
+else
+	echo "alias flutter='/goinfre/$USER/flutter/bin/flutter'" >> ~/.zshrc
+  echo "Created Alias For Flutter"
+fi
+
+if grep -Fq "dart='/goinfre/$USER/flutter/bin/dart'" ~/.zshrc
+then
+  :
+else
+	echo "alias dart='/goinfre/$USER/flutter/bin/dart'" >> ~/.zshrc
+  echo "Created Alias For Dart"
+fi
